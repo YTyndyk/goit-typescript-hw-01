b8d1267efa63324213754b61e33f7136166b92f4
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-09-24 11:15:00 +0300

    task 7-9

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { concatenation } from "./concatenation";
+// import { concatenation } from "./concatenation";
 
-const button = document.querySelector("button")!;
-const input = document.querySelector("input")!;
+// const button = document.querySelector("button")!;
+// const input = document.querySelector("input")!;
 
-if (button && input) {
-	button.addEventListener("click", () => {
-		concatenation(input.value, "hello!");
-	});
+// if (button && input) {
+// 	button.addEventListener("click", () => {
+// 		concatenation(input.value, "hello!");
+// 	});
+// }
+const enum Days {
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday,
 }
+
+const weekend = {
+	saturday: Days.Saturday,
+	sanday: Days.Sunday,
+};
+
+function isWeekend(day: Days) {
+	if (weekend) {
+		return true;
+	}
+}
+isWeekend(Days.Saturday);
